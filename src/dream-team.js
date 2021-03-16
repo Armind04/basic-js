3b714af7dd1d308ce98c7b313811593aa5715fc9
armind04 <purpleskyemusic@gmail.com>
2021-03-16 15:32:26 +0200

    feat:complete dreamteam task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (!Array.isArray(arr)) {
+    return false;
+  }
+
+  let a = arr.reduce((res, current) => {
+    if (typeof current === 'string') {
+      res = res + (current.trim()[0]).toUpperCase();
+    }
+    return res;
+  }, '')
+
+  return a.split('').sort().join('');
 };
