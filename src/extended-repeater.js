a87b378f6338e9b59e62f1a08904ec234d55ebc7
armind04 <purpleskyemusic@gmail.com>
2021-03-22 22:48:15 +0200

    feat:complete repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+   options.addition = '' + options.addition || ''; 
+
+  function arrMaker(count, string) {
+    if (string === 'undefined') return [];
+    let arr = [];
+    for (let i = 0; i < count; i++) { arr.push(string); }
+    return arr; 
+  }
+
+  const a = arrMaker(options.additionRepeatTimes || 1, options.addition);
+
+  const b = arrMaker(options.repeatTimes || 1, `${str}${a.join(options.additionSeparator || '|')}`)
+
+  return b.join(options.separator || '+');
 };
   
